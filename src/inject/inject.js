17058776ff091044995ffb5742d973d2fb23917c
muesugi <moie.uesugi@gmail.com>
2018-07-26 00:23:49 -0700

    initial commit, template

---
+++ src/inject/inject.js
@@ -0,0 +1,14 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+		console.log(document.getElementsByClassName("restaurant"))
+
+		// ----------------------------------------------------------
+		// This part of the script triggers when page is done loading
+		console.log("Hello. This message was sent from scripts/inject.js");
+		// ----------------------------------------------------------
+
+	}
+	}, 10);
+});
